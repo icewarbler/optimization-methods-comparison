df6fd6a0480fbabadd7e680b3b1f85e8d859bb93
icewarbler <beaumagro@gmail.com>
2025-08-03 16:06:03 -0500

    Adds golden section search

--- methods/cg.py
+++ methods/cg.py
@@ -1,4 +1,17 @@
 import numpy as np
 
+def golden_section_search(fun, a, b):
+    while abs(a-b) > tol:
+        tau = (np.sqrt(5) - 1) / 2
+        m1 = a + (1-tau)*(b-a)
+        m2 = a + tau*(b-a)
+
+        if fun(m1) > fun(m2):
+            a = m1
+        else:
+            b = m2
+
+    return a
+    
 def run(f, grad_f, x0, max_iter=1000):
     return 0
